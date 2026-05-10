96170d1136823121745af161fbbdf7775a95b5bc
DAV200689 <ma277751@students.cavendish.ac.ug>
2026-05-10 14:50:16 -0700

    Add greeting logic for names starting with 'J'

--- script.js
+++ script.js
@@ -153,3 +153,17 @@
 // console.log("after b.x update");
 // console.log(a);
 // console.log(b);
+
+
+
+let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (let i = 0; i < names.length; i++) {
+    let firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+        console.log("Goodbye " + names[i]);
+    } else {
+        console.log("Hello " + names[i]);
+    }
+}
