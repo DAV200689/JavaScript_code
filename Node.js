12442be736547c5c83a9a4aa0c51f333a1735918
DAV200689 <ma277751@students.cavendish.ac.ug>
2026-05-25 13:51:59 -0700

    Add initial Node.js code examples and comments

---
+++ Node.js
@@ -0,0 +1,96 @@
+// Variables
+// const balance = 9000;
+// const description = "Salary";
+// const newTransaction = true;
+
+// console.log(balance);
+
+// data types & structures(string, number, boolean, array, object)
+
+// const text = "hello Papa";
+// const number = 2000;
+// const isTrue = true;
+// const myArray = [1, 2, 3];
+// const transaction = { 
+//     description: "salary", 
+//     amount: 3000,
+//  };
+// console.log(text);
+// console.log(number);
+// console.log(isTrue);
+// console.log(myArray.length);
+// console.log(myArray[2]);
+// console.log(transaction);
+// console.log(transaction.description);
+// console.log(transaction.amount);
+
+// Functions (function traditional vs arrow function)
+
+// function calculateBalance() {
+//     let income = 40000;
+//     let expenses = 30000;
+//     let balance = income - expenses;
+//     return balance;
+// }
+// console.log(calculateBalance());
+// function calculateBalance(income, expenses) {
+//      let balance = income - expenses;
+//     return balance; 
+
+//     console.log(`Your balance is ${balance}`);
+// }
+//     console.log(calculateBalance(40000, 30000));
+
+// function expression
+// var calculateBalance = function(income, expenses) {
+//     let balance = income - expenses;
+//     return balance; 
+// }   
+// console.log(calculateBalance(60000, 30000));
+// Arrow function
+// const calculateBalanceArrow = (income, expenses) => {
+//     let balance = income - expenses;
+//     return balance; 
+// }   
+// console.log(calculateBalanceArrow(70000, 50000));
+
+// Interacting with HTML and CSS (DOM manipulation)
+// const balanceElement = document.querySelector(".first-heading");
+// balanceElement.textContent = "gfdsasd";
+
+// const BalanceE1 = document.querySelector(".balance-number");
+
+// BalanceE1.addEventListener("click", function () {
+//     const balance = 5000 - 3000;
+//     BalanceE1.textContent = balance;
+// });
+
+// const BalanceE1 = document.querySelector(".balance-number");
+
+// BalanceE1.addEventListener("click", function () {
+//     BalanceE1.style.color = "Blue";
+// });
+
+// scoping & hoisting
+
+// calculateBalance();
+// function calculateBalance() {
+//     const a= 300;
+//     console.log(a);
+// }
+
+
+// string concatenation & template literals
+
+// const BalanceNumber = 6000;
+// const BalanceText = `Your balance is: ${BalanceNumber}`;  // const BalanceText = "Your balance is: " + BalanceNumber;
+// console.log(BalanceText);
+
+
+// control flow
+// const balance = 4000;
+// if (balance >0) {
+//     console.log("positive");
+// } else {
+//     console.log("negative");
+// }
